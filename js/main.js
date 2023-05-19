9d8e374ddfd1a6f48e10689e020e28feffbab3e7
Matheus Rodrigues <rodriguesmfc03@gmail.com>
2023-05-19 15:45:24 -0300

    otimizei o código js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 //enquanto
-while(contador < listaDeTeclas.length) {
+/* while(contador < listaDeTeclas.length) {
 
     const tecla = listaDeTeclas[contador]
     const instrumento = tecla.classList[1];
@@ -23,4 +23,20 @@ while(contador < listaDeTeclas.length) {
     contador = contador + 1;
 
     //console.log(contador);
+} */
+
+//para
+for(let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1];
+    //template string
+    const idAudio = `#som_${instrumento}`;
+    
+    //console.log(idAudio);
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    }
 }
+
